deb1e0be6fab52ff23b9b96ad02cdd6f0a09bd95
Ann Yesip <esip.ann@gmail.com>
2021-11-01 13:26:06 +0200

    Initial Commit

---
+++ RegistrationPageDesign/RegistrationPageDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationPageDesignApp.swift
+//  RegistrationPageDesign
+//
+//  Created by Ann Yesip on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationPageDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
